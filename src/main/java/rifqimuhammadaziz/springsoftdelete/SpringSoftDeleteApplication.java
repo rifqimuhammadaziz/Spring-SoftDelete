b172b5beeba74fecb8c03f58baa88b2b1e4d96bd
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-05-23 07:59:34 +0700

    Create SoftDelete Spring Project

---
+++ src/main/java/rifqimuhammadaziz/springsoftdelete/SpringSoftDeleteApplication.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.springsoftdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoftDeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSoftDeleteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rifqimuhammadaziz/springsoftdelete/SpringSoftDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.springsoftdelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoftDeleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
